6dc9317f5d114386b08d987fbd77328a8e95bc61
pimentellima <matheus_dere@hotmail.com>
2023-06-13 08:48:41 -0300

    correção url prod

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,25 +5,25 @@ import axios from "axios";
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    jwt: async({ token, user, trigger, session }) => {
-      if(trigger === 'update') {
-        token.name = session.name
-        token.imageUrl = session.imageUrl
+    jwt: async ({ token, user, trigger, session }) => {
+      if (trigger === "update") {
+        token.name = session.name;
+        token.imageUrl = session.imageUrl;
       }
-      if(user) {
-        token.id = user.id
-        token.username = user.username
-        token.name = user.name
-        token.imageUrl = user.imageUrl
+      if (user) {
+        token.id = user.id;
+        token.username = user.username;
+        token.name = user.name;
+        token.imageUrl = user.imageUrl;
       }
-      return token
+      return token;
     },
     session: async ({ session, token }) => {
       if (token) {
-        session.user.id = token.id
-        session.user.username = token.username
-        session.user.name = token.name
-        session.user.imageUrl = token.imageUrl
+        session.user.id = token.id;
+        session.user.username = token.username;
+        session.user.name = token.name;
+        session.user.imageUrl = token.imageUrl;
       }
       return session;
     },
@@ -31,14 +31,17 @@ export const authOptions = {
   pages: {
     signIn: "/signin",
     signUp: "/signup",
-    error: '/error'
+    error: "/error",
   },
   providers: [
     CredentialsProvider({
       name: "Credentials",
       authorize: async (credentials, req) => {
-        const res = await axios.post("http://localhost:3000/api/auth/signin", credentials);
-        if(res.data) return res.data;
+        const res = await axios.post(
+          process.env.WEBSITE_URL + "api/auth/signin",
+          credentials
+        );
+        if (res.data) return res.data;
         return null;
       },
     }),
